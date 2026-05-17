3c74224f02af408c57b2567e0f2ae56aad8460e3
Svitlana <svetagimish@gmail.com>
2026-05-17 17:27:56 +0300

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+import { checkSession } from "./lib/api/serverApi";
 
-const PRIVATE_ROUTES = ["/profile/:path*", "/notes/:path*"];
+const PRIVATE_ROUTES = ["/profile", "/notes"];
 const AUTH_ROUTES = ["/sign-in", "/sign-up"];
 
 const isPrivateRoute = (pathname: string) =>
@@ -14,12 +15,28 @@ export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
 
-  if (isPrivateRoute(pathname) && !accessToken && !refreshToken) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+  if (isPrivateRoute(pathname)) {
+    if (!accessToken && !refreshToken) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    if (!accessToken && refreshToken) {
+      try {
+        const response = await checkSession();
+        const newAccessToken = response.headers["set-cookie"];
+        const nextResponse = NextResponse.next();
+        if (newAccessToken) {
+          nextResponse.headers.set("set-cookie", newAccessToken.toString());
+        }
+        return nextResponse;
+      } catch {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+      }
+    }
   }
 
   if (isAuthRoute(pathname) && (accessToken || refreshToken)) {
-    return NextResponse.redirect(new URL("/profile", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
